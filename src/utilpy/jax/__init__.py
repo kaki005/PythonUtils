8ea8e0b3417dfd9b67513281233fe2b3bb63335e
kaki005 <u097498f@ecs.osaka-u.ac.jp>
2024-11-26 09:24:30 +0900

    [jax] add householder

--- src/utilpy/jax/__init__.py
+++ src/utilpy/jax/__init__.py
@@ -1,3 +1,3 @@
 from .format import format_pytree, format_pytreedef
 from .func import log_universal
-from .linalg import PSD_inv
+from .linalg import householder, inv, orthogonal_matrix
--- src/utilpy/jax/linalg.py
+++ src/utilpy/jax/linalg.py
@@ -1,18 +1,39 @@
+import equinox as eqx
+import jax
 import jax.numpy as jnp
 from jax import jit
-from jax.numpy.linalg import cholesky, inv
+from jax.numpy.linalg import cholesky
+from jax.scipy.linalg import cho_factor, cho_solve
+from jaxtyping import Array, Float
 
 
 @jit
-def PSD_inv(matrix: jnp.ndarray) -> jnp.ndarray:
-    """inverse of Positive semi-Definite matrix. (O(N^2))
-
-    Args:
-        matrix (jnp.ndarray): PSD matrix
-
-    Returns:
-        jnp.ndarray: inverse of PSD matrix
+def inv(P):
     """
-    L = cholesky(matrix)
-    Linv = inv(L)
-    return Linv.T @ Linv
+    Compute the inverse of a PSD matrix using the Cholesky factorisation
+    """
+    L = cho_factor(P, lower=True)
+    return cho_solve(L, jnp.eye(P.shape[-1]))
+
+
+@eqx.filter_jit
+def householder(v: jnp.ndarray, D: int) -> Float[Array, "D D"]:
+    sign = jnp.sign(v[0])
+    k = len(v)
+    v = v.at[0].add(sign * jnp.linalg.norm(v))
+    u = v / jnp.linalg.norm(v)
+    tilde_H = -sign * (jnp.eye(k, dtype=jnp.double) - 2 * jnp.outer(u, u.T))
+    if k == D:
+        H = tilde_H
+    else:
+        H = jax.scipy.linalg.block_diag(jnp.eye(D - k), tilde_H)
+    return H
+
+
+@eqx.filter_jit
+def orthogonal_matrix(vs: list[jnp.ndarray], D: int, Q: int) -> Float[Array, "D Q"]:
+    U = jnp.eye(D, dtype=jnp.double)
+    for i, v in enumerate(vs):
+        H_n = householder(v, D)
+        U = U @ H_n
+    return U[:, :Q]
