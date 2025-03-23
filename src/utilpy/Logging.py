972ede747d57f337f739fa6d5e2a0d1fdae12a75
kaki005 <u097498f@ecs.osaka-u.ac.jp>
2025-03-23 23:05:00 +0900

    [log] add catppuccin theme

--- src/utilpy/Logging.py
+++ src/utilpy/Logging.py
@@ -1,7 +1,10 @@
 import logging
 
 import rich.pretty as pretty
+from catppuccin.extras.rich_ctp import frappe, latte, macchiato, mocha
+from rich.console import Console
 from rich.logging import RichHandler
+from rich.theme import Theme
 
 
 class OriginalFormatter(logging.Formatter):
@@ -9,8 +12,9 @@ class OriginalFormatter(logging.Formatter):
         return pretty.Pretty(record.msg)
 
 
-def log_init(level=logging.INFO):
-    handler = RichHandler(markup=True, rich_tracebacks=True)
+def log_init(theme: Theme | None = mocha, level=logging.INFO):
+    console = Console(theme=theme) if theme is not None else None
+    handler = RichHandler(console=console, markup=True, rich_tracebacks=True)
     # handler.setFormatter(OriginalFormatter())
     logging.basicConfig(
         level=level,
