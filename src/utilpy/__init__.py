3735f0fd92f5b7656bc6ddf48116ccf508723f20
kaki005 <u097498f@ecs.osaka-u.ac.jp>
2025-07-25 11:15:38 +0900

    add set_major_tick_fixed

--- src/utilpy/__init__.py
+++ src/utilpy/__init__.py
@@ -2,7 +2,7 @@ from .cd_diagram import draw_cd_diagram
 from .dataset import _convert_tcp_flag_to_int, kagglehub_dir, to_datetime
 from .laptime import LapTime, lap_time
 from .Logging import log_init
-from .plot import plot_pr_curve, set_major_tick_per_day, set_minor_tick, set_major_tick_fixed
+from .plot import plot_pr_curve, set_major_tick_fixed, set_major_tick_per_day, set_minor_tick
 from .RyeHelper import RyeHelper
 from .string_builder import StringBuilder
 from .tree import format_directory
