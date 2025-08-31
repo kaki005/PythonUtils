357952e5ea4806613c37f78fa2c66c3f00b2e165
kaki005 <u097498f@ecs.osaka-u.ac.jp>
2025-08-31 11:55:48 +0900

    add set_major_tick_per_year

--- src/utilpy/__init__.py
+++ src/utilpy/__init__.py
@@ -11,7 +11,14 @@ from .dataset import (
 from .laptime import LapTime, lap_time
 from .Logging import log_init
 from .matrix import load_sparse_matrix, save_sparse_matrix
-from .plot import plot_pr_curve, set_major_tick_fixed, set_major_tick_per_day, set_major_tick_per_month, set_minor_tick
+from .plot import (
+    plot_pr_curve,
+    set_major_tick_fixed,
+    set_major_tick_per_day,
+    set_major_tick_per_month,
+    set_major_tick_per_year,
+    set_minor_tick,
+)
 from .RyeHelper import RyeHelper
 from .string_builder import StringBuilder
 from .tree import format_directory
--- src/utilpy/plot.py
+++ src/utilpy/plot.py
@@ -26,6 +26,36 @@ def plot_pr_curve(fig_path: Path, true_labels: np.ndarray, anomaly_scores: np.nd
     return pr_auc, precision, recall, thresholds
 
 
+def set_major_tick_per_year(
+    ax: Axes,
+    timeColumn: pd.DatetimeIndex | pd.Series | np.ndarray | None = None,
+    start: datetime | None = None,
+    end: datetime | None = None,
+    label_loc: float = -0.03,
+    rotation: int = 0,
+    format: str = "%y",
+    tick_collor: str = "black",
+    tick_linestyle: str = "--",
+):
+    if timeColumn is not None and isinstance(timeColumn, np.ndarray):
+        timeColumn = pd.Series(timeColumn)
+    if start is None:
+        assert timeColumn is not None
+        start = (
+            timeColumn.min().replace(year=1, day=1, hour=0, minute=0, second=0, microsecond=0).to_pydatetime()
+        )  # 開始を日付の始まりに設定
+    if end is None:
+        assert timeColumn is not None
+        end = pd.to_datetime(timeColumn.max()).to_pydatetime()
+    dayLocator = mdates.YearLocator()
+    ax.xaxis.set_major_locator(dayLocator)
+    ax.xaxis.set_major_formatter(mdates.DateFormatter(format))
+    for tick in dayLocator.tick_values(start, end):  # 日付の変わり目に
+        ax.axvline(x=tick, color=tick_collor, linestyle=tick_linestyle, lw=0.5)
+    _set_major_tick_pos(ax, label_loc, rotation)
+    return start, end
+
+
 def set_major_tick_per_month(
     ax: Axes,
     timeColumn: pd.DatetimeIndex | pd.Series | np.ndarray | None = None,
@@ -33,7 +63,7 @@ def set_major_tick_per_month(
     end: datetime | None = None,
     label_loc: float = -0.03,
     rotation: int = 0,
-    format: str = "%Y-%m",  # Changed to year-month format
+    format: str = "%m",  # Changed to year-month format
     tick_collor: str = "black",
     tick_linestyle: str = "--",
 ):
